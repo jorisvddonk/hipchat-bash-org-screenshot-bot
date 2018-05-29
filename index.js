f07e14ca9a0fb2032de0d619f5d82adb3de82a8c
joris <jorisvddonk@gmail.com>
2018-05-29 21:38:52 +0200

    Implement functionality!

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-var express = require('express');
-var app = express();
 var port = process.env.PORT || 3000;
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.listen(port);
 
-app.get('/hello', function (req, res) {
-    res.send({'qs': req.query});
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const imgur = require("imgur");
+
+app.use(bodyParser.json());
+app.post("/bash-screenshot", (req, res) => {
+  console.log(req.body);
+
+  var imgur = require("imgur");
+  var bashScreenshot = require("bash-screenshotter");
+  bashScreenshot(1)
+    .then(function(data) {
+      return imgur
+        .uploadBase64(data[0].toString("base64"))
+        .then(function(json) {
+          return json.data.link;
+        });
+    })
+    .catch(function(error) {
+      var retval = {
+        color: "red",
+        message: `Error: ${error}`,
+        notify: false,
+        message_format: "text"
+      };
+      console.error(error);
+      res.send(JSON.stringify(retval));
+    })
+    .then(function(imgururl) {
+      var retval = {
+        color: "green",
+        message: `<img src="${imgururl}"/>`,
+        notify: false,
+        message_format: "html"
+      };
+      console.log(retval);
+      res.send(JSON.stringify(retval));
+    });
 });
 
-console.log('Server started on: ' + port);
+app.listen(port, () => console.log("Server started on port", port));
